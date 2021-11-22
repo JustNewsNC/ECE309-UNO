aafaae86cf95428b2bac61f29b6c01d8276b5da5
naydt01 <58788092+naydt01@users.noreply.github.com>
2021-11-22 14:18:59 -0500

    new files

---
+++ UNOfuns.h
@@ -0,0 +1,8 @@
+//
+// Created by naydt on 11/22/2021.
+//
+
+#ifndef ECE309_UNO_UNOFUNS_H
+#define ECE309_UNO_UNOFUNS_H
+
+#endif //ECE309_UNO_UNOFUNS_H
---
+++ UNOgame.cpp
@@ -0,0 +1,5 @@
+//
+// Created by naydt on 11/22/2021.
+//
+include UNOfuns.h
+
