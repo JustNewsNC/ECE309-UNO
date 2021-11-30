dff6dbb6d99d720218fcb190d90ab8f644a3aee9
naydt01 <58788092+naydt01@users.noreply.github.com>
2021-11-29 20:12:48 -0500

    working on Deal and Draw

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 vector<Player> playerList;
+DrawPile* drawstack = new DrawPile();
+PlayPile* playstack = new PlayPile();
 
 //fills a vector with the specified amount of bots
 void createPlayers(int amount){
@@ -24,7 +26,7 @@ void GameStartWithPlayer(){
     cout << "Welcome! What is your name?" << endl;
     cin >> name;
     playerList.push_back(RealPlayer(name));
-    cout << "Are you readY to play, " << playerList.front().name << "? (Y/N)" << endl;
+    cout << "Are you Ready to play, " << playerList.front().name << "? (Y/N)" << endl;
     cin >> input;
 
     int playerCount = 0;
@@ -43,11 +45,23 @@ void GameStartNoPlayer(){
     createPlayers(playerCount); //need to initialize playerCount
 }
 
+void Hand::Draw() {
+    cards.push_back(drawstack->cards[0]);
+
+}
+
+void DrawPile::Deal() {
+    for(int i=0; i<7; i++) {
+        for(int j=0; j<(int)playerList.size(); j++) {
+            playerList[j].currentCards.Draw();
+        }
+    }
+}
 
 int main() {
-    /*int turnCount = 1;
+    int turnCount = 1;
     char input;
-    bool winner;
+    bool winner = false;
     cout << "Are you a player or spectator? (P/S)" << endl;
     cin >> input;
     if(input == 'P'){
@@ -55,26 +69,24 @@ int main() {
     }
     else {
         GameStartNoPlayer();
-    }*/
-    DrawPile* draw = new DrawPile();
-    draw->Deal();
-    /*PlayPile* play = new PlayPile();
+    }
+    drawstack->Deal();
     int playerIndex = 0;
     Player* currentPlayer = nullptr;
     
     while(!winner){
         cout << "Turn " << turnCount << ". -------------" << endl;
-        cout << "The card on top is: " << play->PrintTop() << endl;
-        playerIndex = currentTurn % playerList.size; //Finds the remainder on the turn count to shortcut keeping track of the player list
-        currentPlayer = playerList[playerIndex];
+        cout << "The card on top is: "; playstack->PrintTop(); cout << endl;
+        playerIndex = turnCount % playerList.size(); //Finds the remainder on the turn count to shortcut keeping track of the player list
+        currentPlayer = &playerList[playerIndex];
         cout << "It is " << currentPlayer->name << "'s turn" << endl;
         currentPlayer->play(); //play function differs between AI and humans
-        cout << currentPlayer->name << " played " << play->PrintTop() << endl;
+        cout << currentPlayer->name << " played "; playstack->PrintTop(); cout << endl;
         if(currentPlayer->currentCards.length == 0){
             cout << currentPlayer->name << " has won!" << endl;
             winner = true;
         }
-    }*/
+    }
     return 0;
 }
 
